ef86d0b75c2bd7a4867277d8bd9c4da513bda5e1
HassanShanJava <hassanshan675@gmail.com>
2023-02-20 03:01:11 +0500

    working on the header component

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,11 +1,13 @@
+import { Box, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>
-        
-    </div>
-  )
+    <Box textAlign={"center"} bg={"#6B46C1"} pt={"90px"} pb="100px  " color="white"> 
+        <Heading>Simple pricing for your business</Heading>
+        <Text pt="10px">Plans that are carefully crafted to suit your business.</Text>
+    </Box>
+   )
 }
 
 export default Header
