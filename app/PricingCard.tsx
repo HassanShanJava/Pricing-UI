a359188ca12bd0bc6b66f809b5038f7734711607
HassanShanJava <hassanshan675@gmail.com>
2023-02-25 04:47:33 +0500

    working ga-on pricing card

--- app/PricingCard.tsx
+++ app/PricingCard.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Box, Flex } from "@chakra-ui/react";
+import React from "react";
 
 const PricingCard = () => {
   return (
-    <div>PricingCard</div>
-  )
-}
+    <Box>
+      <Flex>
+        <Box>b1</Box>
+        <Box>b2</Box>
+      </Flex>
+    </Box>
+  );
+};
 
-export default PricingCard
+export default PricingCard;
