f1bae7f4c9d7414b56a76bed650f772458b88926
HassanShanJava <hassanshan675@gmail.com>
2023-02-25 05:20:48 +0500

    working ga-on pricing card

--- app/PricingCard.tsx
+++ app/PricingCard.tsx
@@ -1,11 +1,18 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
 
 const PricingCard = () => {
   return (
-    <Box>
-      <Flex>
-        <Box>b1</Box>
+    <Box >
+      <Flex >
+        <Box bg={"#f0eafb"} p={"60px"}  textAlign={"center"}>
+          <Text fontWeight={"bold"} fontSize={"24px"}>
+            Premium PRO
+          </Text>
+          <Heading fontSize={"60px"}>$329</Heading>
+          <Text>billed just once</Text>
+          <Button mt={"20px"} w={"300px"} bg={"#805AD5"} color={"white"}>Get Started</Button>
+        </Box>
         <Box>b2</Box>
       </Flex>
     </Box>
